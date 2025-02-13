513ecb383bb35a51d7ca314ecbe39f2104639b09
dadysayid <dadisayid19@gmail.com>
2025-02-13 13:00:59 +0100

    create-websit-layout

---
+++ lib/data.ts
@@ -0,0 +1,37 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Today's deal",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'New Arrivals',
+      href: '/search?tag=new-arrivals',
+    },
+    {
+      name: 'Features Products',
+      href: '/search?tag=feature-product',
+    },
+    {
+      name: 'Best sealer',
+      href: '/search?tag=best-sealer',
+    },
+    {
+      name: 'Browsing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+    {
+      name: 'Help',
+      href: '/page/help',
+    },
+  ],
+}
+export default data
